2eaea6ce1cd19aaf135219c41cca6a961381974b
neogt <neozomber5551@gmail.com>
2025-09-29 22:44:54 -0600

    first commit

---
+++ Rest/code.js
@@ -0,0 +1,17 @@
+const nuevoPost = {
+  title: "Mi primer post",
+  body: "Contenido del post",
+  userId: 1
+};
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer 12345abcde"
+  },
+  body: JSON.stringify(nuevoPost)
+})
+.then(res => res.json())
+.then(data => console.log("Respuesta del servidor:", data))
+.catch(err => console.error(err));
