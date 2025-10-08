24c985ec1c2292d41811f532e99221b550b94bd8
Noe <neozomber5551@gmail.com>
2025-10-07 23:20:13 -0600

    appReproductor Java

---
+++ reproductor/src/main/java/com/mycompany/reproductor/Reproductor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reproductor;
+
+/**
+ *
+ * @author neogt
+ */
+public class Reproductor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
